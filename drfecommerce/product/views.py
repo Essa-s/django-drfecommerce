afb081c80357a86cf07b6fa4be9a637aee7e611a
Essa-s <essa4a@gmail.com>
2023-12-25 18:05:57 +0200

    add views

--- drfecommerce/product/views.py
+++ drfecommerce/product/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Category
+from .serializers import CategorySerializer
+from drf_spectacular.utils import extend_schema
 
-# Create your views here.
+class CategoryView(viewsets.ViewSet):
+    """
+    a simple viewset for viewing category
+    """
+
+    queryset = Category.objects.all()
+    @extend_schema(responses=CategorySerializer)
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
